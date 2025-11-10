13c55bdc5455dc712ace1552e758ca6070fc2600
liumeng <1685281215@qq.com>
2025-11-10 16:56:34 +0800

    feat: 点击按钮生成随机数0-9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,10 @@ android {
         version = release(36)
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.lm.myapplication"
         minSdk = 24
--- app/src/main/java/com/lm/myapplication/MainActivity.kt
+++ app/src/main/java/com/lm/myapplication/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.lm.myapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.lm.myapplication.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //初始化binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //设置内容试图的root
+        setContentView(binding.root)
+
+        //button设置点击事件监听
+        binding.actionButton.setOnClickListener {
+            var randomNum = Random.nextInt(10)
+            //当button被点击时，执行
+            binding.infoTextView.text = randomNum.toString()
         }
     }
 }
